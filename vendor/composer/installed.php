42588eb9179acb313cd7db68966c6886ede4be07
shayan vosoughi <shayanwqhw@gmail.com>
2022-10-11 17:36:29 +0330

    perf: :zap: Change Contact List Theme

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'shayandev/cms-painter',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'b81f05db3f9b971c2a826aec360fdcf504736ded',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'shayandev/cms-painter' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'b81f05db3f9b971c2a826aec360fdcf504736ded',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
